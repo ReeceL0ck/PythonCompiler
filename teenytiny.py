6ceb1e2617f8ffb92cf9f686b41c35dd3efa4940
Reece Lock <reece.lock55@gmail.com>
2025-08-12 08:54:46 +0100

    Added extention handling

--- teenytiny.py
+++ teenytiny.py
@@ -8,9 +8,11 @@ def main():
 
     if len(sys.argv) != 2:
         sys.exit("Error: Compiler needs a source file")
+    if sys.argv[1][-5:] != 'teeny':
+        sys.exit(f'Error: Invalid file extension - {sys.argv[1]} does not end in .teeny')
     with open(sys.argv[1],'r') as inputFile:
         source = inputFile.read()
-
+        
     lexer = Lexer(source)
     emitter = Emitter('out.c')
     parser = Parser(lexer, emitter=emitter)
